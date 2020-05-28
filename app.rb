c52d1668580395b000fbac032bd79c99821f06f3
Rachael <rachaelewins@hotmail.com>
2020-05-28 16:08:03 +0100

    refactor, Post-Get

--- app.rb
+++ app.rb
@@ -2,14 +2,21 @@ require 'sinatra'
 
 class Battle < Sinatra::Base
 
+  enable :sessions
 
   get '/' do
     erb(:form)
   end
 
   post '/names' do
-    @player_1 = params[:player_1]
-    @player_2 = params[:player_2]
-    erb(:names)
+    session[:player_1] = params[:player_1]
+    session[:player_2] = params[:player_2]
+    redirect('/play')
+  end
+
+  get '/play' do
+    @player_1 = session[:player_1]
+    @player_2 = session[:player_2]
+    erb(:play)
   end
 end
