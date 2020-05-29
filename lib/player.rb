0f4d739c887235217bf73ea8b78cf2256dd81c8d
Sophie Worsnop <sophie.g.worsnop@gmail.com>
2020-05-29 15:39:41 +0100

    Add attack and deduct hp to player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,20 @@
 class Player
-  attr_reader :name
-  def initialize(name)
+
+  DEFAULT_HP = 100
+
+  attr_reader :name, :hp
+
+  def initialize(name, hp = DEFAULT_HP)
     @name = name
+    @hp = hp
+  end
+
+  def deduct_hp
+    @hp -= 10
+  end
+
+  def attack(player)
+    player.deduct_hp
   end
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,7 +1,26 @@
 require 'player'
 describe Player do
   let(:player_1) {Player.new("Bene")}
-  it 'has a name' do
+  let(:player_2) {Player.new("Sophie")}
+
+  it 'players have names' do
     expect(player_1.name).to eq("Bene")
+    expect(player_2.name).to eq("Sophie")
+  end
+
+  it 'creates an new instance of player with default HP of 100' do
+    expect(player_1.hp).to eq(100)
+  end
+
+  describe "#deduct_hp" do
+    it 'reduces the players HP by 10' do
+      expect{ player_1.deduct_hp }.to change { player_1.hp}.by -10
+    end
+  end
+
+  describe '#attack' do
+    it 'player 1 attacks player and reduces HP by 10' do
+      expect{ player_1.attack(player_2) }.to change { player_2.hp}.by -10
+    end
   end
 end
